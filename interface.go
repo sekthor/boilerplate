b0250bf1d1c2d6e91956c54b6450d66871845e59
sekthor <sekthor@protonmail.ch>
2024-12-18 19:14:59 +0100

    fix: add missing interceptor method

--- interface.go
+++ interface.go
@@ -16,6 +16,7 @@ type BoilerplateServer interface {
 	WithGrpcRegisterFunc(GrpcRegisterFunc) *boilerplate
 	WithGatewayRegisterFunc(GatewayRegisterFunc) *boilerplate
 	WithTracer(string) *boilerplate
+	WithJwks(jwksUrls []string) *boilerplate
 	RegisterGateway(GatewayRegisterFunc)
 	RegisterGrpc(GrpcRegisterFunc)
 	Run(context.Context) error
