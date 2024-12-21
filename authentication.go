f17e78858a80d29359f5dcff03ebba96f3650854
sekthor <sekthor@protonmail.ch>
2024-12-21 10:51:08 +0100

    feat: add user.id attribute

--- authentication.go
+++ authentication.go
@@ -6,6 +6,8 @@ import (
 
 	"github.com/MicahParks/keyfunc/v3"
 	"github.com/golang-jwt/jwt/v5"
+	"go.opentelemetry.io/otel/attribute"
+	"go.opentelemetry.io/otel/trace"
 	"google.golang.org/grpc"
 	"google.golang.org/grpc/metadata"
 )
@@ -40,6 +42,8 @@ func UnaryJwtInterceptor(jwksUrls []string) (grpc.UnaryServerInterceptor, error)
 			return nil, errInvalidToken
 		}
 
+		span := trace.SpanFromContext(ctx)
+		span.SetAttributes(attribute.String("user.id", claims.Subject))
 		ctx = context.WithValue(ctx, "claims", claims)
 
 		return handler(ctx, req)
